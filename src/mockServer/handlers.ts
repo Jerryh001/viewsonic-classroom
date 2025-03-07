ce4234fd89d7d094c675bda9c8721d56a4192690
Jerryh001 <jerryh001@hotmail.com>
2025-03-07 15:09:33 +0800

    feat: add mocked backend responses

--- src/mockServer/handlers.ts
+++ src/mockServer/handlers.ts
@@ -1,27 +1,29 @@
 import { http, HttpResponse } from "msw";
 import { backendUrl } from "./util";
+import { classRoom, classRoomStudents } from "./mockData";
+import { API } from "../types/api";
 
 export const handlers = [
-    http.get<{ id: string }, null, {} | null>(
-        backendUrl("/classroom/:id"),
+    http.get<{ id: string }, null, API.ClassRoom.Get.Response>(
+        backendUrl("/classrooms/:id"),
         ({ params }) => {
             if (params.id === undefined) {
                 return HttpResponse.json(null, { status: 400 });
             }
             // I assume the request is correct since it just a mock server
 
-            return HttpResponse.json({});
+            return HttpResponse.json(classRoom);
         }
     ),
-    http.get<{ id: string }, null, string[] | null>(
-        backendUrl("/classroom/:id/students"),
+    http.get<{ id: string }, null, API.ClassRoom.ListStudents.Response>(
+        backendUrl("/classrooms/:id/students"),
         ({ params }) => {
             if (params.id === undefined) {
                 return HttpResponse.json(null, { status: 400 });
             }
             // I assume the request is correct since it just a mock server
 
-            return HttpResponse.json([]);
+            return HttpResponse.json(classRoomStudents);
         }
     ),
 ];
